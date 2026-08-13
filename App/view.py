d6e21c780413d03ac22a53a384605b0f71d0931c
Samuel Roncancio Pava <samuel.roncancio@uniandes.edu.co>
2026-08-12 23:40:32 -0500

    Entrega Final - Laboratorio 1

--- App/view.py
+++ App/view.py
@@ -50,16 +50,11 @@ def new_logic():
     return app
 
 
-def print_menu():
-    """
-    Imprime el menú de opciones en consola para el usuario
-    """
+def printMenu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TODO: Mods Lab 1, agregar la opcion 3.
-    # Agregue la opción 3 para cargar los tags de los libros.
-    # Pueede guiarse de las opciones 1 y 2.
+    print("3- Cargar Tags de los libros")
     print("0- Salir")
 
 
@@ -128,7 +123,10 @@ def main():
             print("Cargando información de tags....")
             tags = load_tags(app)
             print("Total de tags cargados: " + str(tags) + "\n")
-
+        elif int(inputs[0]) == 3:
+          print("Cargando información de tags de los libros....")
+          booksTags = load_books_tags(app)
+          print("Total de tags de los libros cargados: " + str(booksTags) + "\n")
         # TODO: Mods Lab 1, agregar la funcion opt 3 -> load_book_tags().
         # Agregue la opción 3 que llama a la función load_books_tags() (creada en la lógica).
         # Esta función carga los tags de los libros en el catalogo.
